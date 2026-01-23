693ed432f8040fa1a0b3b14d8a2b816ce8cac3f7
Guilherme23645 <gui.depaula236@gmail.com>
2026-01-22 23:48:11 -0300

    chore: add comments that will help understand and come up with StartupBust implementation

--- src/StartupTestDrive.java
+++ src/StartupTestDrive.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 
 public class StartupTestDrive {
     public static void main(String[] args) {
+        // Game Setup
         ArrayList<Startup> startups = new ArrayList<>();
         Startup startup1 = new Startup();
         Startup startup2 = new Startup();
@@ -15,6 +16,7 @@ public class StartupTestDrive {
         startup2.setName("hacqi");
         startup3.setName("cabista");
 
+        // Placing Startups
         ArrayList<String> arr1 = new ArrayList<>();
         arr1.add("G0");
         arr1.add("G1");
@@ -34,12 +36,13 @@ public class StartupTestDrive {
         startup2.setLocationCells(arr2);
         startup3.setLocationCells(arr3);
 
+        // Creating Guesses and Checking Them
         String guess1 = "C2";
         String guess2 = "D2";
         String guess3 = "E2";
 
         startup2.checkYourself(guess1);
         startup2.checkYourself(guess2);
-        System.out.println(startup2.checkYourself(guess3));
+        System.out.println(startup2.checkYourself(guess3)); // This should output "kill"
     }
 }
