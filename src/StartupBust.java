bc5d4dc9434342b7e541def6c81d4c35c977290d
Guilherme23645 <gui.depaula236@gmail.com>
2026-01-25 05:47:16 -0300

    final

--- src/StartupBust.java
+++ src/StartupBust.java
@@ -1,23 +1,72 @@
+
+import java.util.ArrayList;
+
 public class StartupBust {
-    public static void main(String[] args) {
-        int numOfGuesses = 0;
-        GameHelper helper = new GameHelper();
+    private final GameHelper helper = new GameHelper();
+    private final ArrayList<Startup> startups = new ArrayList<>();
+    private int numOfGuesses;
 
-        SimpleStartup theStartup = new SimpleStartup();
-        int randomNum = (int) (Math.random() * 5);
+    public void setUpGame() {
+        Startup one = new Startup();
+        one.setName("poniez");
+        Startup two = new Startup();
+        two.setName("hacqi");
+        Startup three = new Startup();
+        three.setName("cabista");
+        startups.add(one);
+        startups.add(two);
+        startups.add(three);
 
-        int[] locations = {randomNum, randomNum + 1, randomNum + 2};
-        theStartup.setLocationCells(locations);
-        boolean isAlive = true;
+        System.out.println("Your goal is to sink three Startups.");
+        System.out.println("poniez, hacqi, cabista");
+        System.out.println("Try to sink them all in the fewest number of guesses");
 
-        while (isAlive) {
-            int guess = helper.getUserInput("enter a number");
-            String result = theStartup.checkYourself(guess);
-            numOfGuesses++;
+        for (Startup startup : startups) {
+            ArrayList<String> newLocation = helper.placeStartup(3);
+            startup.setLocationCells(newLocation);
+        }
+    }
+
+    public void startPlaying() {
+        while (!startups.isEmpty()) {
+            String userGuess = helper.getUserInput("Enter a guess");
+            checkUserGuess(userGuess);
+        }
+        finishGame();
+    }
+
+    public void checkUserGuess(String userGuess) {
+        numOfGuesses++;
+        String result = "miss";
+
+        for (Startup startupToTest : startups) {
+            result = startupToTest.checkYourself(userGuess);
+
+            if (result.equals("hit")) {
+                break;
+            }
             if (result.equals("kill")) {
-                isAlive = false;
-                System.out.println("You took " + numOfGuesses + " guesses");
+                startups.remove(startupToTest);
+                break;
             }
         }
+        System.out.println(result);
+    }
+
+    public void finishGame() {
+        System.out.println("All Startups are dead! Your stock is now worthless");
+        if (numOfGuesses <= 18) {
+            System.out.println("It only took you " + numOfGuesses + " guesses.");
+            System.out.println("You got out before your options sank.");
+        } else {
+            System.out.println("Took you long enough. " + numOfGuesses + " guesses.");
+            System.out.println("Fish are dancing with your options");
+        }
+    }
+
+    public static void main(String[] args) {
+        StartupBust game = new StartupBust();
+        game.setUpGame();
+        game.startPlaying();
     }
 }
